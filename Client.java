520816b4453e2be833d28edb8d8a0101ae1ce9ce
gantalikhitha <105198094+gantalikhitha@users.noreply.github.com>
2022-05-11 15:31:39 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,29 @@
+package com.bean.Annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Client {
+	
+		
+	
+	public static void main(String[] args) {
+	
+		
+//		AnnotationConfigApplicationContext con=new AnnotationConfigApplicationContext(StudentConfiguration.class);
+		
+		AnnotationConfigApplicationContext con=new AnnotationConfigApplicationContext(StudentConfiguration.class);
+		System.out.println("java is loaded");
+		Student stu=con.getBean("student",Student.class);
+		System.out.println("stu object is created");
+		System.out.println("hello");
+		stu.display();
+		con.close();
+		}
+		
+		
+			
+	
+
+}
+
